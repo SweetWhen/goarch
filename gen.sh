961c248ce7b20859448dbe740e79db3d131e3ac2
whensweet <whensweetsemail>
2023-01-02 17:28:08 +0800

    fix: gen and import correct

--- gen.sh
+++ gen.sh
@@ -2,8 +2,10 @@
 
 rm -rf custompb/proto/gen/*
 
-#pbDir="custompb/proto"
-protoc --go_out=./ custompb/proto/interceptor/*.proto
-protoc --go_out=./ --go-grpc_out=./  custompb/proto/helloworld/*.proto
+#--go_opt=module=$prefix gen pb.go to another path
+#--go_opt=paths=source_relative gen pb.go to the path the same with .proto file
+protoc --go_out=./custompb --go_opt=module=github.com/SweetWhen/goarch/custompb custompb/proto/interceptor/*.proto
+protoc --go_out=./custompb --go_opt=module=github.com/SweetWhen/goarch/custompb \
+        --go-grpc_out=./custompb --go-grpc_opt=module=github.com/SweetWhen/goarch/custompb custompb/proto/helloworld/*.proto
 
 
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/SweetWhen/goarch
 go 1.16
 
 require (
+	github.com/mohuishou/protoc-gen-go-gin v0.1.0 // indirect
 	google.golang.org/grpc v1.51.0
 	google.golang.org/protobuf v1.28.1
 )
